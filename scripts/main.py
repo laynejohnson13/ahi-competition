3c686c5cba70f326b7a3efdf7547082d61092318
laynejohnson13 <laynejohnson13001@gmail.com>
2022-11-23 11:45:23 -0500

    cleaned external csv created

--- scripts/main.py
+++ scripts/main.py
@@ -1,16 +1,44 @@
 import pandas as pd
+import streamlit as st
+import numpy as np
 
-df = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/HHA-507-2022/main/competition/data/output_noshow1_balanced_2022-11-16.csv')
-df
+##importing ORIGINAL dataset
+original = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/HHA-507-2022/main/competition/data/output_noshow1_balanced_2022-11-16.csv')
+original
 
 #Viewing data 
 
-df.columns
-df.shape
-df.dtypes 
+original.columns
+original.shape
+original.dtypes 
 
 
-print("Given Dataframe :\n", df) 
+print("Given Dataframe :\n", original) 
 
-age = df(df['patient_gender'], 'female')
+age = original(original['patient_gender'], 'female')
 print('\nResult dataframe :\n', age)
+
+
+
+###importing 
+
+external = pd.read_csv("data/external_data.csv")
+external 
+
+##Viewing data
+external.columns
+external.shape
+external.dtypes 
+
+##important factors to note: alcholism, diabtetes, hypertension, age, gender
+
+##cleaning external columns
+
+external.columns = external.columns.str.replace('[^A-Za-z0-9]+', '_')
+external.columns = external.columns.str.lower()
+
+##renaming external columns 
+renamed_external = external.rename(columns={'alcoolism': 'alcoholism', 'hipertension': 'hypertension'})
+
+#exporting cleaned csv
+renamed_external.to_csv("data/cleaned_external.csv")
